c962611d6d5453e5419e454f0d14e4977191fc7f
StevenJV <steven@vorefamily.net>
2018-08-23 14:10:19 -0400

    builder for a couple of our projects

---
+++ buildetl.ps1
@@ -0,0 +1,27 @@
+function buildthem
+{
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [String] $path
+    )
+    process
+    {
+        $msBuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe'
+        
+        Write-Host "Cleaning $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Clean /verbosity:quiet /property:Configuration=Release
+
+        Write-Host "Building $($path)" -ForegroundColor Green
+        & "$($msBuildExe)" "$($path)" /t:Build /verbosity:quiet /property:Configuration=Release
+    }
+}
+
+cd \code\insights\etl
+$EtlDirectory = "C:\code\insights\etl"
+$SchemaPath = Join-Path -Path $EtlDirectory -ChildPath "Navex.Insights.Schema\Navex.Insights.Schema.csproj"
+buildthem $SchemaPath 
+$PackageBuilderPath = Join-Path -Path $EtlDirectory -ChildPath "Navex.Insights.Etl.SsisPackageBuilder\Navex.Insights.Etl.SsisPackageBuilder.csproj"
+buildthem $PackageBuilderPath
+Navex.Insights.Etl.Schema\bin\Debug\Navex.Insights.Etl.Schema.exe --SourceDb=cm_Test1 --SourceServer=. --RunStaging
+Navex.Insights.Etl.SsisPackageBuilder\bin\Debug\Navex.Insights.Etl.SsisPackageBuilder.exe --CustomerKey=test1 --SourceDb=cm_Test1
