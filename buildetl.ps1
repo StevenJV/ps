14e4bca09b2dd504d8dfd13ec542d57d6e319a39
StevenJV <steven@vorefamily.net>
2019-01-10 15:44:19 -0500

    cleanup messaging

--- buildetl.ps1
+++ buildetl.ps1
@@ -9,7 +9,7 @@ function buildthem
     {
         $msBuildExe = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe'
         
-        Write-Host "Cleaning $($path)" -foregroundcolor green
+        Write-Host "Cleaning $($path)" -foregroundcolor Yellow
         & "$($msBuildExe)" "$($path)" /t:Clean /verbosity:quiet /property:Configuration=Release
 
         Write-Host "Building $($path)" -ForegroundColor Green
@@ -21,7 +21,6 @@ $EtlDirectory = "$PSScriptRoot\..\insights\etl\"
 Set-Location $EtlDirectory
 Get-ChildItem $EtlDirectory -Recurse -Filter *.csproj | 
 Foreach-Object {
-  Write-Host "building $_.FullName" -foregroundcolor green
   buildthem $_.FullName
 }
 Write-Host "running Schema.exe --SourceDb=cm_Test1 --SourceServer=. --RunStaging --StagingServer=. --StagingDb=Insights_Staging" -foregroundcolor green
