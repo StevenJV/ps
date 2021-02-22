0d592ec79cd84c02c464ccc5dc6f8186dcf78ff7
StevenJV <steven@vorefamily.net>
2021-02-22 11:04:29 -0500

    updates to services needed/desired

--- stopinsights.ps1
+++ stopinsights.ps1
@@ -1,12 +1,12 @@
 Write-Host "stopping Insights services" -foregroundcolor blue -BackgroundColor White
-#net stop navex.intake.processor
-#net stop navex.distribution.processor
-net stop navex.insights.queueconsumer
-net stop navex.insights.TransformationConsumer
-net stop navex.insights.EtlConsumer
-net stop navex.insights.scheduler
-net stop navex.insights.CdsService
-net stop Navex.insights.CaseTransformationService
-net stop Navex.insights.PlatformUserTransformationService
+START-SERVICE navex.intake.processor
+START-SERVICE navex.distribution.processor
+START-SERVICE navex.insights.queueconsumer
+START-SERVICE navex.insights.TransformationConsumer
+START-SERVICE navex.insights.EtlConsumer
+START-SERVICE navex.insights.scheduler
+START-SERVICE navex.insights.CdsService
+START-SERVICE Navex.insights.CaseTransformationService
+START-SERVICE Navex.insights.PlatformUserTransformationService
 Write-Host "stopping IIS" -foregroundcolor blue -BackgroundColor White
 iisreset /stop
