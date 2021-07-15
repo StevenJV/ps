24e487385b7494989cb3020918adb75b1a9f1601
StevenJV <steven@vorefamily.net>
2021-07-15 11:17:38 -0400

    fixup paths

--- buildetl.ps1
+++ buildetl.ps1
@@ -17,10 +17,9 @@ function buildthem
     }
 }
 
-$EtlDirectory = "$PSScriptRoot\..\insights\etl\"
-$migrationDirectory = "$PSScriptRoot\..\insights\API\Navex.Insights.Migrations\bin\Release\netcoreapp3.1"
-
-Push-Location $EtlDirectory
+$EtlDirectory = "$PSScriptRoot\..\insights\etl"
+$InsightsDirectory =  "$PSScriptRoot\..\insights"
+Push-Location $InsightsDirectory
 
 Write-Host "building old etl, ssis" -foregroundcolor Yellow
 Get-ChildItem $EtlDirectory -Recurse -Filter *.csproj |
@@ -28,23 +27,17 @@ Foreach-Object {
   buildthem $_.FullName
 }
 
-Write-Host "building new migrator" -foregroundcolor Yellow
-Push-Location $migrationDirectory
-Get-ChildItem $migrationDirectory -Recurse -Filter *.csproj |
-Foreach-Object {
-  buildthem $_.FullName
-}
-Pop-Location
-
 Write-Host "running Schema.exe --SourceDb=cm_Test1" -foregroundcolor green
-Navex.Insights.Etl.Schema\bin\Release\Navex.Insights.Etl.Schema.exe --SourceDb=cm_Test1
+. $EtlDirectory\Navex.Insights.Etl.Schema\bin\Release\Navex.Insights.Etl.Schema.exe --SourceDb=cm_Test1
 
 Write-Host "Schema.exe --SourceDb=cm_insightsqalocal1" -ForegroundColor green
-Navex.Insights.Etl.Schema\bin\Release\Navex.Insights.Etl.Schema.exe --SourceDb=cm_insightsqalocal1
+. $EtlDirectory\Navex.Insights.Etl.Schema\bin\Release\Navex.Insights.Etl.Schema.exe --SourceDb=cm_insightsqalocal1
+
+Write-Host "Schema.exe --SourceDb=cm_tophat" -ForegroundColor green
+. $EtlDirectory\Navex.Insights.Etl.Schema\bin\Release\Navex.Insights.Etl.Schema.exe --SourceDb=cm_tophat --SourceServer=.
+
 Write-Host "running Migration" -foregroundcolor green
-. $migrationDirectory\Navex.Insights.Migrations.exe
+. tools\PsScripts\runMigrations.ps1
 
-Write-Host "running SsisPackageBuilder.exe --CustomerKey=test1 --SourceDb=cm_Test1" -foregroundcolor green
-Navex.Insights.Etl.SsisPackageBuilder\bin\Release\Navex.Insights.Etl.SsisPackageBuilder.exe --CustomerKey=test1 --SourceDb=cm_Test1 --PackageDirectory C:\code\insights\etl\Navex.Insights.Ssis
-
-Pop-Location
+Write-Host "running SsisPackageBuilder.exe to C:\Deploy\SsisPackages --CustomerKey=test1 --SourceDb=cm_Test1" -foregroundcolor green
+. $ETLdirectory\Navex.Insights.Etl.SsisPackageBuilder\bin\Release\Navex.Insights.Etl.SsisPackageBuilder.exe --CustomerKey=test1 --SourceDb=cm_Test1 --PackageDirectory="C:\Deploy\SsisPackages"
